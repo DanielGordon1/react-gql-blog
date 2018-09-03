29c1749317dc49325ea019c07b2df8b673a0fae6
Daniel Gordon <d_gordon1@icloud.com>
2018-09-03 13:53:35 +0200

    Add models and db layer.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Post.create([
+  {title: "5 Steps to going overtime in your first presentation", content: "Try to explain a bunch of concepts and do a live code portion. Becausde that's not scary at all!"},
+  {title: "Tips for getting to pet every dog you pass on the street", content: "As soon as you're in eyesight of the human, show excitment so they know they're gonna need to stop for you."}
+])
